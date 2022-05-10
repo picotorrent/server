ab3cd6262a9ad9fdfa3cfe580a69f796fff38a75
Viktor Elofsson <viktor@viktorelofsson.se>
2022-05-10 20:25:54 +0200

    Client styling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,33 @@
 import './App.css';
-import { Box, Flex, HStack, IconButton, Image, Input, InputGroup, InputLeftElement, InputRightElement, Kbd, Menu, MenuButton } from '@chakra-ui/react';
+import { Box, CircularProgress, CircularProgressLabel, Flex, HStack, IconButton, Image, Input, InputGroup, InputLeftElement, InputRightElement, Kbd, Menu, MenuButton, Progress, Text } from '@chakra-ui/react';
 import { HamburgerIcon, SearchIcon } from '@chakra-ui/icons';
+
+interface Torrent {
+  name: string;
+}
+
+interface TorrentItem {
+  item: Torrent;
+}
+
+function TorrentListItem(props: TorrentItem) {
+  return (
+    <Box className='TorrentListItem' p='2' borderColor={'gray.200'} borderRadius='lg' borderWidth={'thin'}>
+      <Text fontSize='sm'>
+        {props.item.name}
+      </Text>
+      <Progress value={20} size='xs' />
+    </Box>
+  )
+}
 
 function App() {
   fetch('/api/jsonrpc').then(console.log).catch(console.error);
 
   return (
-    <Flex mt={20} justifyContent={'center'}>
+    <Flex mt='20px' justifyContent={'center'}>
       <Box w={600}>
-        <Flex alignItems={'center'} borderBottom={'1px solid #f0f0f0'} pb={'3'}>
+        <Flex alignItems={'center'} borderBottom={'1px solid #f0f0f0'} mb='3' pb={'3'}>
           <Box flex={0.5}>
             <Image src='/logo.png' boxSize={'40px'} />
           </Box>
@@ -39,6 +58,9 @@ function App() {
             </Menu>
           </Flex>
         </Flex>
+        <TorrentListItem item={{
+          name: 'ubuntu-21.04-desktop-amd64.iso'
+        }} />
       </Box>
     </Flex>
   );
