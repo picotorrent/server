c871d7e2ccaa4865c932bb2391bad66682eeeafc
Viktor Elofsson <viktor@viktorelofsson.se>
2022-05-10 19:53:14 +0200

    Update client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Box, Flex, HStack, IconButton, Image, Input, InputGroup, InputLeftElement, InputRightElement, Kbd, Menu, MenuButton } from '@chakra-ui/react';
+import { HamburgerIcon, SearchIcon } from '@chakra-ui/icons';
 
 function App() {
+  fetch('/api/jsonrpc').then(console.log).catch(console.error);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Flex mt={20} justifyContent={'center'}>
+      <Box w={600}>
+        <Flex alignItems={'center'} borderBottom={'1px solid #f0f0f0'} pb={'3'}>
+          <Box flex={0.5}>
+            <Image src='/logo.png' boxSize={'40px'} />
+          </Box>
+          <Box flex={5}>
+            <InputGroup>
+              <InputLeftElement
+                pointerEvents={'none'}
+                children={<SearchIcon color={'gray.300'} />}
+              />
+              <Input placeholder='Pika command palette ...' />
+              <InputRightElement pointerEvents={'none'} width='6em'>
+                <HStack>
+                  <Kbd>Ctrl</Kbd>
+                  <Kbd>K</Kbd>
+                </HStack>
+              </InputRightElement>
+            </InputGroup>
+          </Box>
+          <Flex justifyContent={'end'} flex={0.5}>
+            <Menu>
+              <MenuButton
+                as={IconButton}
+                aria-label='Menu'
+                icon={<HamburgerIcon />}
+                size='sm'
+                variant='outline'
+              />
+            </Menu>
+          </Flex>
+        </Flex>
+      </Box>
+    </Flex>
   );
 }
 
