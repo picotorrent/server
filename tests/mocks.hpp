9e17a970bb9b454afc554a24e3f9e7dd05f18670
Viktor Elofsson <viktor@viktorelofsson.se>
2022-07-07 13:21:36 +0200

    Fix unit tests

--- tests/mocks.hpp
+++ tests/mocks.hpp
@@ -11,6 +11,7 @@ class MockSession : public pika::ISession
 public:
     MOCK_METHOD(void, AddEventHandler, (std::weak_ptr<pika::ISessionEventHandler> handler));
     MOCK_METHOD(libtorrent::info_hash_t, AddTorrent, (const libtorrent::add_torrent_params &params));
+    MOCK_METHOD((std::map<std::string, int64_t>), Counters, ());
     MOCK_METHOD(std::shared_ptr<pika::ITorrentHandle>, FindTorrent, (const libtorrent::info_hash_t& hash));
     MOCK_METHOD(void, ForEachTorrent, (const std::function<void(const libtorrent::torrent_status&)> &cb));
     MOCK_METHOD(void, RemoveTorrent, (libtorrent::info_hash_t const& hash, bool removeFiles));
